5ad681615b240eaf5e0c24b5216b8af46fed72e7
Mans Magnusson <mons.magnusson@gmail.com>
2024-01-11 20:13:15 +0100

    Minor fix

--- RPackage/tests/testthat/test-date_to_publyearmonth.R
+++ RPackage/tests/testthat/test-date_to_publyearmonth.R
@@ -1,7 +1,7 @@
 
-context("SwedishPolls data")
+context("publyearmonth_to_date")
 
-test_that(desc="get_polls()",{
+test_that(desc="publyearmonth_to_date()",{
   x <- c("2018-aug","1973-okt", "1973-aug", "1973-jun", "1973-maj", "1973-apr", "1973-mar", "1973-feb", "1973-jan", "1972-dec", "1972-nov", "1972-okt", "1972-sep")
   x2 <- expect_silent(publyearmonth_to_date(x))
   x3 <- expect_silent(date_to_publyearmonth(x2))
